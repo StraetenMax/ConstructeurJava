03d43055e33634ca7d7d443c654aa81fc49d3281
StraetenMax <maxstraeten@yahoo.com>
2017-03-23 20:58:17 +0100

    encapsulation

---
+++ src/com/teachersdunet/javaintermediaire1/Voiture.java
@@ -0,0 +1,17 @@
+package com.teachersdunet.javaintermediaire1;
+
+public class Voiture {
+	
+	public void demarrer(){
+		
+	}
+	public void klaxonner(){
+		System.out.println("PinPinPin !!!");
+	}
+	public void stop(){
+		
+	}
+	protected void relierFilBleuAuFilNoir(){
+		
+	}
+}
---
+++ src/com/teachersdunet/javaintermediaire1/VoitureTest.java
@@ -0,0 +1,9 @@
+package com.teachersdunet.javaintermediaire1;
+
+public class VoitureTest {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
