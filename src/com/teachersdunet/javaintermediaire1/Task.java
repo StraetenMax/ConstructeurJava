97dd06d4c89f0fb7a90db65bbce77727821e6d22
StraetenMax <maxstraeten@yahoo.com>
2017-03-23 17:39:43 +0100

    finalisation du constructeur

--- src/com/teachersdunet/javaintermediaire1/Task.java
+++ src/com/teachersdunet/javaintermediaire1/Task.java
@@ -2,5 +2,9 @@ package com.teachersdunet.javaintermediaire1;
 
 public class Task {
 	public String description;
-	public boolean completed;
+	public boolean completed = false;
+	
+	public Task(String desc){
+		description = desc;
+	}
 }
--- src/com/teachersdunet/javaintermediaire1/TaskTest.java
+++ src/com/teachersdunet/javaintermediaire1/TaskTest.java
@@ -3,7 +3,7 @@ package com.teachersdunet.javaintermediaire1;
 public class TaskTest {
 
 	public static void main(String[] args) {
-		Task tache1 = new Task();
+		Task tache1 = new Task("faire un tutoriel");
 		System.out.println(tache1.description);
 
 	}
