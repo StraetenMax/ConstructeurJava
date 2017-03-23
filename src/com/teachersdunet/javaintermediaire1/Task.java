66c2ad0d6e767992ec28c8d0f86130be229bcec9
StraetenMax <maxstraeten@yahoo.com>
2017-03-23 18:02:20 +0100

    complexifier l'exercice en ajoutant un attribut

--- src/com/teachersdunet/javaintermediaire1/Task.java
+++ src/com/teachersdunet/javaintermediaire1/Task.java
@@ -1,10 +1,16 @@
 package com.teachersdunet.javaintermediaire1;
 
 public class Task {
+	public String titre;
 	public String description;
 	public boolean completed = false;
 	
-	public Task(String desc){
+	public Task(String title, String desc){
+		titre = title;
 		description = desc;
+	}
+	
+	public void complete (){
+		completed = true;
 	}
 }
--- src/com/teachersdunet/javaintermediaire1/TaskTest.java
+++ src/com/teachersdunet/javaintermediaire1/TaskTest.java
@@ -3,8 +3,16 @@ package com.teachersdunet.javaintermediaire1;
 public class TaskTest {
 
 	public static void main(String[] args) {
-		Task tache1 = new Task("faire un tutoriel");
+		Task tache1 = new Task("Tutoriel","Faire un tutoriel en peu d'heure");
+		Task tache2 = new Task("Cinéma","Allons au cinéma ce soir");
+		Task tache3 = new Task("Exécuter une action", "Dire mamamya à la fin de la vidéo");
+		
+		tache2.complete();
+		
+		
+		System.out.println(tache1.titre);
 		System.out.println(tache1.description);
+		System.out.println(tache2.completed);
 
 	}
 
