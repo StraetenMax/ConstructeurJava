1545ce38a83d8f5fffeae09a97cd8864acf8e86c
StraetenMax <maxstraeten@yahoo.com>
2017-03-23 21:43:50 +0100

    différence entre public private protected

---
+++ src/com/teacherdunet/javaintermediaire2/ClasseB.java
@@ -0,0 +1,10 @@
+package com.teacherdunet.javaintermediaire2;
+  
+import com.teachersdunet.javaintermediaire1.ClasseA;
+
+public class ClasseB {
+public static void main(String[] args) {
+	ClasseA a = new ClasseA();
+	a.maMethode();
+}
+}
---
+++ src/com/teachersdunet/javaintermediaire1/ClasseA.java
@@ -0,0 +1,11 @@
+package com.teachersdunet.javaintermediaire1;
+
+public class ClasseA {
+	public int entier1;
+	public float reel1;
+	
+	public void maMethode(){
+		System.out.println("je suis ma methode");
+	}
+	
+}
