660300889fad9f6c543e91001cf47d59e55a56b2
StraetenMax <maxstraeten@yahoo.com>
2017-03-23 17:11:42 +0100

    creation du dossier d'exercie

---
+++ src/com/teachersdunet/javaintermediaire1/Task.java
@@ -0,0 +1,6 @@
+package com.teachersdunet.javaintermediaire1;
+
+public class Task {
+	public String description;
+	public boolean completed;
+}
---
+++ src/com/teachersdunet/javaintermediaire1/TaskTest.java
@@ -0,0 +1,11 @@
+package com.teachersdunet.javaintermediaire1;
+
+public class TaskTest {
+
+	public static void main(String[] args) {
+		Task tache1 = new Task();
+		System.out.println(tache1.description);
+
+	}
+
+}
