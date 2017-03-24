544651cec4d552b792138af48d74da9764facb21
StraetenMax <maxstraeten@yahoo.com>
2017-03-24 06:39:41 +0100

    construction d'une boîte de dialogue

---
+++ src/com/teachersdunet/javaintermediaire1/BoiteDialogueTest.java
@@ -0,0 +1,11 @@
+package com.teachersdunet.javaintermediaire1;
+
+import javax.swing.JOptionPane;
+
+public class BoiteDialogueTest {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null,"Salut les amis !");
+	}
+
+}
