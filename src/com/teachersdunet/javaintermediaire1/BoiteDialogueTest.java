5103652186e4efe2fab3602a14e8fa33e99f319e
StraetenMax <maxstraeten@yahoo.com>
2017-03-24 06:57:17 +0100

    boite de dialogue plus sophistiquée

--- src/com/teachersdunet/javaintermediaire1/BoiteDialogueTest.java
+++ src/com/teachersdunet/javaintermediaire1/BoiteDialogueTest.java
@@ -8,7 +8,11 @@ public class BoiteDialogueTest {
 		
 		String nom =JOptionPane.showInputDialog("Quel est ton nom ?");
 		
-		JOptionPane.showMessageDialog(null,"Bonjour "+nom);
-	}
+		String message = String.format("Bonjour %s. Java est cool n'est-ce pas?", nom);
+		//JOptionPane.showMessageDialog(null,"Bonjour "+nom);
+		JOptionPane.showMessageDialog(null, message);
 
 }
+	}
+
+	
