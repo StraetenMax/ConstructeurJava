6acc03d15543b392bd83ccf3bc1edf46446ad78f
StraetenMax <maxstraeten@yahoo.com>
2017-03-24 06:46:56 +0100

    boite sophistiquée

--- src/com/teachersdunet/javaintermediaire1/BoiteDialogueTest.java
+++ src/com/teachersdunet/javaintermediaire1/BoiteDialogueTest.java
@@ -5,7 +5,10 @@ import javax.swing.JOptionPane;
 public class BoiteDialogueTest {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null,"Salut les amis !");
+		
+		String nom =JOptionPane.showInputDialog("Quel est ton nom ?");
+		
+		JOptionPane.showMessageDialog(null,"Bonjour "+nom);
 	}
 
 }
