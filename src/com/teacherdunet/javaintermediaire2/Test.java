933cdbf61b2776bca400080cec92c4148f3d81c3
StraetenMax <maxstraeten@yahoo.com>
2017-03-25 17:26:17 +0100

    import static

--- src/com/teacherdunet/javaintermediaire2/Test.java
+++ src/com/teacherdunet/javaintermediaire2/Test.java
@@ -1,17 +1,21 @@
 package com.teacherdunet.javaintermediaire2;
 
+import static java.lang.Math.abs;
+
+
 public abstract class Test {
 
 	public static void main(String[] args) {
-		System.out.println(Math.abs(-15));
+		//System.out.println(Math.abs(-15));
+		System.out.println(abs(-25));
 	}
-	public int abs(int value){
+	/*public static int abs(int value){
 		if(value >=0){
 			return value;
 		}else{
 			return -value;
 		}
-	}
+	}*/
 	
 	
 	
