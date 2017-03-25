10629c4874d7cdebb636cc8ed5f79cfd4bf85cab
StraetenMax <maxstraeten@yahoo.com>
2017-03-25 17:45:05 +0100

    Pour en finir avec les imports Static

--- src/com/teacherdunet/javaintermediaire2/Test.java
+++ src/com/teacherdunet/javaintermediaire2/Test.java
@@ -1,13 +1,19 @@
 package com.teacherdunet.javaintermediaire2;
 
-import static java.lang.Math.abs;
-
+//import static java.lang.Math.abs;
+//import static java.lang.Math.cos;
+//import static java.lang.Math.PI;
+//import static java.lang.Math.sin;
+import static java.lang.Math.*;
 
 public abstract class Test {
 
 	public static void main(String[] args) {
 		//System.out.println(Math.abs(-15));
 		System.out.println(abs(-25));
+		System.out.println(cos(0));
+		System.out.println(sin(28));
+		System.out.println(PI);
 	}
 	/*public static int abs(int value){
 		if(value >=0){
